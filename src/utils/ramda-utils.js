96bc079c7a71c513f18f77608e7f793e089cfe5c
Devonte <devo.tox.89@gmail.com>
2016-12-03 02:02:35 +0000

    pass tests

--- src/utils/ramda-utils.js
+++ src/utils/ramda-utils.js
@@ -10,11 +10,13 @@ const chainP = curry((fn, arr) => Promise.all(chain(fn, arr)));
 // toArray :: { a: b } -> [{ k: a, v: b }]
 const toArray = compose(map(zipObj(['key', 'value'])), toPairs);
 
+// jshint ignore: start
 // FutureFromPromise :: ( () -> Promise e a ) -> Future e a
 const FutureFromPromise = (fn: (*) => Future) => Future((reject, resolve) => {
 	fn().then(resolve, reject);
 	return () => {};
 });
+// jshint ignore: end
 
 // innerJoin :: String -> String -> [a] -> [b] -> [{String:a}]
 const innerJoin = curry((k1, k2, l1, l2) => {
@@ -28,4 +30,6 @@ const log = curry((msg, v) => {
     return v;
 });
 
+// jshint ignore: start
 module.exports = { log, chainP, toArray, innerJoin, Future, FutureFromPromise };
+// jshint ignore: end
